ab8d73a02d2967f5031af47d3763f0f68350c48b
KRIShark <cristofnagy@gmail.com>
2023-02-04 18:59:50 +0100

    Update doorlock.lua

--- doorlock.lua
+++ doorlock.lua
@@ -1,11 +1,13 @@
 local code = "2023"
 local input = read("Enter code: ")
 
-if input == code then
-    print("Access granted. Door opened.")
-    redstone.setOutput("left", true)
-    sleep(10)
-    redstone.setOutput("left", false)
-else
-    print("Access denied.")
+while true do
+    if input == code then
+        print("Access granted. Door opened.")
+        redstone.setOutput("left", true)
+        sleep(10)
+        redstone.setOutput("left", false)
+    else
+        print("Access denied.")
+    end
 end
